76c183d43d0bce72c2d3e3f2dfbf49f39681ac14
Snowy <snowy@totodile.io>
2020-12-25 20:34:25 -0600

    Add the static linking stuff here too

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,17 @@
 #include <QStyle>
 #include <QScreen>
 #include <QIcon>
+
+#ifdef SIX_SHOOTER_WIN32_EXE_STATIC_LINK
+#include <QtPlugin>
+#endif
+
 #include "main_window.hpp"
+
+#ifdef SIX_SHOOTER_WIN32_EXE_STATIC_LINK
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin)
+#endif
 
 int main(int argc, char **argv) {
     QApplication a(argc, argv);
